dbb818f12f9e7d6f7f51da1146ae3869185d394a
jortizme <jortizmez@gmail.com>
2020-05-10 22:43:07 +0200

    the whole kern implementation works perfectly, coding in human readable table is what is left to do

--- src/main.c
+++ src/main.c
@@ -11,13 +11,13 @@
 #include <unistd.h>
 #include <time.h>
 
-#define error_several(x) fprintf(stderr,x); exit(EXIT_FAILURE) 
+#define ErrorSeveral(x) fprintf(stderr,x); exit(EXIT_FAILURE) 
 
-#define control_negative(y,x) if(x == -1){\
+#define ControlReturnNegative(y,x) if(x == -1){\
                         fprintf(stderr,"%s failed at line %d of file %s (function %s)\n",y,__LINE__,__FILE__,__func__);\
                         exit(EXIT_FAILURE);}
 
-#define control_null(y,x) if(x == NULL){ \
+#define ControlReturnNULL(y,x) if(x == NULL){ \
                         fprintf(stderr,"%s failed at line %d of file %s (function %s)\n",y,__LINE__,__FILE__,__func__);\
                         exit(EXIT_FAILURE);}
 
@@ -71,30 +71,34 @@ int main(int argc, char *argv[])
 
     getFilesInfo(&forSearch, &Info, &Arguments);
 
+    printf("\n\n");
+
     exit(EXIT_SUCCESS);
 }
 
 void parse_arguments(CommandInfo* Info,int argc,char** argv)
 {
+    //if no arguments and no path given
     if(argc == 1)
     {
         Info->NrArguments = 0;
         Info->PathIsThere = false;
         strcpy(Info->PathName, ".");
     }
+    //if there was something given
     else
     {
         unsigned int cnt;
         unsigned int loopLimit;
 
-        //There was no path given
+        //if there was no path given
         if (strchr(argv[argc-1],'/') == NULL && strchr(argv[argc-1],'.') == NULL)
         {  
             Info->PathIsThere = false;
             strcpy(Info->PathName, ".");
             loopLimit = argc - 1;
         }
-        //There is a path 
+        //if there is a path 
         else 
         {
             Info->PathIsThere = true;
@@ -102,7 +106,7 @@ void parse_arguments(CommandInfo* Info,int argc,char** argv)
             strcat(Info->PathName,"/");
             loopLimit = argc - 2;
 
-            //This is the case, when only the path is available
+            //This is the case, when only the path is available and no arguments
             if(argc == 2)
             {
                 Info->NrArguments = 0;
@@ -110,19 +114,21 @@ void parse_arguments(CommandInfo* Info,int argc,char** argv)
             }
         }
         for(cnt = 1; cnt <= loopLimit; cnt++)
-            {
+            {   
+                //make sure the arguments start with the '-' symbol
                 if(strchr(argv[cnt],'-') == NULL)
                 {
-                    error_several("Arguments must be started by the '-' symbol\n");
+                    ErrorSeveral("Arguments must be started by the '-' symbol\n");
                 }
-
+                
+                //the ++ allows us to ignore the '-' symbol from the argument
                 strcat(Info->Arguments,++argv[cnt]);
             }
         Info->NrArguments = strlen(Info->Arguments);
 
         if(Info->NrArguments > 4)
         {
-            error_several("Too much arguments given, maximal 4 are allowed\n");
+            ErrorSeveral("Too much arguments given, maximal 4 are allowed\n");
         }   
     }
 
@@ -137,7 +143,7 @@ void parse_arguments(CommandInfo* Info,int argc,char** argv)
 
 void getFilesInfo(VariablesforSearch* forSearch, CommandInfo* Info, CheckForArguments *Arguments)
 {
-    
+    //Argaux created in order to let Info->Arguments unchanged for later use
     char* Argaux = Info->Arguments;
 
     for(int i = 0; i < Info->NrArguments; i++)
@@ -154,29 +160,34 @@ void getFilesInfo(VariablesforSearch* forSearch, CommandInfo* Info, CheckForArgu
         Argaux++;
     }
 
+    //open directory
     forSearch->DirStream = opendir(Info->PathName);
-    control_null("opendir()",forSearch->DirStream);
+    ControlReturnNULL("opendir()",forSearch->DirStream);
         
     errno = 0;
+    int iterator = 0;
 
-    //every file and directory that is contained in the given path is going to be shown
+    //every file/directory contained in the path will be shown
     while((forSearch->DirInfo = readdir(forSearch->DirStream)) != NULL)
     {
+        iterator++;
         if(Info->PathIsThere == true)
         {
             strcpy(forSearch->PathNameAux,Info->PathName);
             strcat(forSearch->PathNameAux,forSearch->DirInfo->d_name);
-            control_negative("lstat()",lstat(forSearch->PathNameAux,&forSearch->buf));
-                
+            ControlReturnNegative("lstat()",lstat(forSearch->PathNameAux,&forSearch->buf));
         }
+        //if there is no path, it means we look for infos in the current directory, this is 
+        //why the d_name is given in lstat() directly and not with a followed concatenated path
         else
         {
-            control_negative("lstat()",lstat(forSearch->DirInfo->d_name,&forSearch->buf));
+            ControlReturnNegative("lstat()",lstat(forSearch->DirInfo->d_name,&forSearch->buf));
         }
 
-        //if a .* file there and -a is no set, then continue to the next iteration
+        //ignore the .* files when -a argument not set
         if( *forSearch->DirInfo->d_name == '.' && Arguments->a_isThere == false )
         {
+            iterator--;
             continue;
         }
 
@@ -195,9 +206,9 @@ void getFilesInfo(VariablesforSearch* forSearch, CommandInfo* Info, CheckForArgu
 
         if(Arguments->a_isThere)
         {
-            if(*Argaux == 'a')
+            //Argaux points to the l,o or g argument when a available
+            while(*Argaux == 'a')
                 Argaux++;
-                    
         }
 
         if(Arguments->l_isThere || Arguments->g_isThere || Arguments->o_isThere)
@@ -229,13 +240,28 @@ void getFilesInfo(VariablesforSearch* forSearch, CommandInfo* Info, CheckForArgu
                 
         }
 
-        printf("%s\t",forSearch->DirInfo->d_name);   //Name der Datei
+        
 
-        printf("\n");
+        if(!Arguments->l_isThere && !Arguments->g_isThere && !Arguments->o_isThere)
+        {
+            printf("%-16s\t\t",forSearch->DirInfo->d_name);   //Name der Datei
+
+            if(iterator % 3 == 0)
+                printf("\n");
+        }
+        else
+        {
+            printf("%s\t",forSearch->DirInfo->d_name);   //Name der Datei
+            printf("\n");
+        }
     }
 
-    control_negative("closedir()",closedir(forSearch->DirStream));
+    printf("\n");
 
+    ControlReturnNegative("closedir()",closedir(forSearch->DirStream));
+
+    //check if the rvalue NULL from readdir() was an error
     if(errno != 0)
-        error_several("errno unequal 0, error at readdir()");
+        ErrorSeveral("errno unequal 0, error at readdir()");
+    
 }
